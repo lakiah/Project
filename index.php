056b7cea83251ba5208d5545ae88c29a4a21889f
lakiah <31241409+lakiah@users.noreply.github.com>
2020-11-16 21:00:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo '
+     <form action="main.php" method="post" action="">
+      <label for="fname">Balance:</label>
+      <input type="text" id="bal" name="bal"><br><br>
 
+      <label for="lname">Payment:</label>
+      <input type="text" id="pay" name="pay"><br><br>
+
+      <label for="fname">Date Transfer:</label>
+      <input type="text" id="dttrans" name="dttrans"><br><br>
+
+      <label for="lname">Remaining Amount:</label>
+      <input type="text" id="remamount" name="remamount"><br><br>
+      
+      <input type="submit" value="Submit">
+    </form>
+
+    ';
+?>
+
+
+  </body>
+</html>
